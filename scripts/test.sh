3d08df1f437f091cc7584a90f231ce5e31c8f4d5
abhishekk <ia.malhotra@gmail.com>
2017-07-18 01:36:17 +0530

    update compose.yml

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,11 +3,10 @@
 . /appenv/bin/activate
 
 # Download requirements to build cache
-# pip download -d /build -r requirements_test.txt --no-input
+pip download -d /build -r requirements_test.txt --no-input
 
 # Install application test requirements
-# pip install --no-index -f /build -r requirements_test.txt
-pip install  -r requirements_test.txt
+pip install --no-index -f /build -r requirements_test.txt
 
 echo "finish test.sh"
 
